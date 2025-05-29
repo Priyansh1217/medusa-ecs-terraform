27aa1ed249ffb790b9a4c667234fe22b5e090a12
Priyansh1217 <98013453+Priyansh1217@users.noreply.github.com>
2025-05-29 15:32:43 +0530

    Update backend.tf

--- medusa-ecs-terraform/terraform/backend.tf
+++ medusa-ecs-terraform/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-unique-terraform-state-bucket"
+    bucket         = "your-unique-terraform-state-buckets"
     key            = "medusa/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
